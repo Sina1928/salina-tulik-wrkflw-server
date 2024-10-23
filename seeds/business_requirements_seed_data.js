2b20969392796aad4ac1772c913090c9d319afaa
Salina Tulik <salina.tulik@gmail.com>
2024-10-22 19:23:55 -0600

    add missing id's to components, industries, and business_requirements seed data

--- seeds/business_requirements_seed_data.js
+++ seeds/business_requirements_seed_data.js
@@ -6,8 +6,8 @@ export async function seed(knex) {
   // Deletes ALL existing entries
   await knex("business_requirements").del();
   await knex("business_requirements").insert([
-    { industry_id: 1 }, // Construction Industry
-    { industry_id: 2 }, // Healthcare Industry
-    { industry_id: 15 }, // Beauty Industry
+    { id: 1, industry_id: 1 }, // Construction Industry
+    { id: 2, industry_id: 2 }, // Healthcare Industry
+    { id: 3, industry_id: 15 }, // Beauty Industry
   ]);
 }
--- seeds/components_seed_data.js
+++ seeds/components_seed_data.js
@@ -7,42 +7,52 @@ export async function seed(knex) {
   await knex("components").del();
   await knex("components").insert([
     {
+      id: 1,
       name: "Invoicing",
       description: "Manage invoices and track payments.",
     },
     {
+      id: 2,
       name: "Document Management",
       description: "Organize and store all important documents.",
     },
     {
+      id: 3,
       name: "Safety Compliance",
       description: "Ensure safety protocols and compliance tracking.",
     },
     {
+      id: 4,
       name: "Time Tracking",
       description: "Track employee work hours and project times.",
     },
     {
+      id: 5,
       name: "Payroll",
       description: "Handle employee salaries, deductions, and payouts.",
     },
     {
+      id: 6,
       name: "Project Management",
       description: "Organize and manage project tasks and milestones.",
     },
     {
+      id: 7,
       name: "Marketing Tools",
       description: "Create and manage marketing campaigns and assets.",
     },
     {
+      id: 8,
       name: "Booking System",
       description: "Allow customers to schedule appointments and services.",
     },
     {
+      id: 9,
       name: "Inventory Management",
       description: "Track stock levels and product inventory.",
     },
     {
+      id: 10,
       name: "Client Relationship Management (CRM)",
       description: "Manage client data and interactions.",
     },
